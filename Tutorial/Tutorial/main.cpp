8a2a7df2674fd3a17e6ef75c8b606b5bb95b576d
skyrippax <akbar95bambalesh@mail.ru>
2022-04-28 14:42:10 +0500

    xcode project was added
    We opened another Xcode project within the clone direct way

---
+++ Tutorial/Tutorial/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Tutorial
+//
+//  Created by AKBAR AMINOV on 28/04/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
